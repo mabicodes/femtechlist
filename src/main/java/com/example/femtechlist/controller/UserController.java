c56b83bcb8ab23981459221329dcd08387a50af9
Nicole Uy <marvienicolebtw@hotmail.com>
2021-08-15 14:01:33 +0200

    adding react project to spring

--- src/main/java/com/example/femtechlist/controller/UserController.java
+++ src/main/java/com/example/femtechlist/controller/UserController.java
@@ -4,11 +4,12 @@ import java.util.List;
 import com.example.femtechlist.model.User;
 import com.example.femtechlist.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/")
 
